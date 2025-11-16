cc720d98c5db1da4e5d118b95b4fad6603216485
jeeva2910 <sjeeva2910@gmail.com>
2025-11-16 21:02:20 +0530

    Add files via upload

---
+++ Powof2.java
@@ -0,0 +1,17 @@
+public class Powof2 {
+    public static void main(String[] args) {
+        int num=16;
+        int i=16;
+        while(i<num){
+            i=i*2;
+        }
+        if(i==num){
+            System.out.println("pow of 2");
+        }
+        else{
+            System.out.println("not pow of 2");
+        }
+    }
+    
+}
+
