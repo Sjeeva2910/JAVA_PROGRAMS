4780d2eb0c82755716aff67cd87f578998c1941f
Jeeva S <sjeeva2910@gmail.com>
2026-03-17 20:13:23 +0530

    Add files via upload

---
+++ Fizz.java
@@ -0,0 +1,38 @@
+import java.util.*;
+
+class Fizz {
+
+    public List<String> fizzBuzz(int n) {
+
+        List<String> result = new ArrayList<>();
+
+        for(int i=1;i<=n;i++){
+
+            if(i%15==0){
+                result.add("FizzBuzz");
+            }
+            else if(i%3==0){
+                result.add("Fizz");
+            }
+            else if(i%5==0){
+                result.add("Buzz");
+            }
+            else{
+                result.add(String.valueOf(i));
+            }
+        }
+
+        return result;
+    }
+
+    public static void main(String args[]){
+
+        Fizz obj = new Fizz();
+
+        int n = 15;
+
+        List<String> output = obj.fizzBuzz(n);
+
+        System.out.println(output);
+    }
+}
