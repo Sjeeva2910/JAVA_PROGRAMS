6c8e2dcac99b615b2f6e612709f6adec1e4eb08b
Jeeva S <sjeeva2910@gmail.com>
2026-03-27 20:32:31 +0530

    Add files via upload

---
+++ MatrixSimilarityCheck.java
@@ -0,0 +1,45 @@
+import java.util.*;
+
+public class MatrixSimilarityCheck {
+
+    public static boolean areSimilar(int[][] mat, int k) {
+        int m = mat.length;
+        int n = mat[0].length;
+
+        k = k % n; // reduce extra rotations
+
+        for (int i = 0; i < m; i++) {
+
+            for (int j = 0; j < n; j++) {
+
+                if (i % 2 == 0) {
+                    // even row → left shift
+                    if (mat[i][j] != mat[i][(j + k) % n]) {
+                        return false;
+                    }
+                } else {
+                    // odd row → right shift
+                    if (mat[i][j] != mat[i][(j - k + n) % n]) {
+                        return false;
+                    }
+                }
+            }
+        }
+
+        return true;
+    }
+
+    public static void main(String[] args) {
+
+        int[][] mat = {
+            {1, 2, 3},
+            {4, 5, 6}
+        };
+
+        int k = 1;
+
+        boolean result = areSimilar(mat, k);
+
+        System.out.println(result); // false
+    }
+}
