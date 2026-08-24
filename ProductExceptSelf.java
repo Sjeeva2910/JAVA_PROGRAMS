f01f798c441884db857565b7f3b5017b63d8d256
Jeeva S <sjeeva2910@gmail.com>
2026-08-24 20:00:18 +0530

    Add files via upload

---
+++ ProductExceptSelf.java
@@ -0,0 +1,36 @@
+public class ProductExceptSelf {
+
+    public static int[] productExceptSelf(int[] nums) {
+
+        int[] result = new int[nums.length];
+
+        int product = 1;
+
+        // LEFT PRODUCT
+        for (int i = 0; i < nums.length; i++) {
+            result[i] = product;
+            product = product * nums[i];
+        }
+
+        // RIGHT PRODUCT
+        product = 1;
+
+        for (int i = nums.length - 1; i >= 0; i--) {
+            result[i] = result[i] * product;
+            product = product * nums[i];
+        }
+
+        return result;
+    }
+
+    public static void main(String[] args) {
+
+        int[] nums = {1, 2, 3, 4};
+
+        int[] result = productExceptSelf(nums);
+
+        for (int num : result) {
+            System.out.print(num + " ");
+        }
+    }
+}
