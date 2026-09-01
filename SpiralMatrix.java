71a9f3e2f7f040af112c794e1a5db2952b36582d
Jeeva S <sjeeva2910@gmail.com>
2026-09-01 21:18:23 +0530

    Add files via upload

---
+++ SpiralMatrix.java
@@ -0,0 +1,60 @@
+import java.util.*;
+
+public class SpiralMatrix {
+
+    public static List<Integer> spiralOrder(int[][] matrix) {
+
+        int top = 0;
+        int bottom = matrix.length - 1;
+        int left = 0;
+        int right = matrix[0].length - 1;
+
+        List<Integer> result = new ArrayList<>();
+
+        while (left <= right && top <= bottom) {
+
+            // Left to Right
+            for (int i = left; i <= right; i++) {
+                result.add(matrix[top][i]);
+            }
+            top++;
+
+            // Top to Bottom
+            for (int i = top; i <= bottom; i++) {
+                result.add(matrix[i][right]);
+            }
+            right--;
+
+            // Right to Left
+            if (top <= bottom) {
+                for (int i = right; i >= left; i--) {
+                    result.add(matrix[bottom][i]);
+                }
+                bottom--;
+            }
+
+            // Bottom to Top
+            if (left <= right) {
+                for (int i = bottom; i >= top; i--) {
+                    result.add(matrix[i][left]);
+                }
+                left++;
+            }
+        }
+
+        return result;
+    }
+
+    public static void main(String[] args) {
+
+        int[][] matrix = {
+            {1, 2, 3},
+            {4, 5, 6},
+            {7, 8, 9}
+        };
+
+        List<Integer> result = spiralOrder(matrix);
+
+        System.out.println(result);
+    }
+}
