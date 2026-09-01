956a3eaabe278fdee6c63fcdc37ef3f024bccd0a
Jeeva S <sjeeva2910@gmail.com>
2026-09-01 19:34:51 +0530

    Add files via upload

---
+++ SubarraySumEqualsK.java
@@ -0,0 +1,42 @@
+import java.util.*;
+
+public class SubarraySumEqualsK {
+
+    public static int subarraySum(int[] nums, int k) {
+
+        int count = 0;
+
+        for (int i = 0; i < nums.length; i++) {
+
+            int sum = 0;
+
+            for (int j = i; j < nums.length; j++) {
+
+                sum = sum + nums[j];
+
+                if (sum == k) {
+                    count++;
+                }
+            }
+        }
+
+        return count;
+    }
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+
+        int[] nums = new int[n];
+
+        for (int i = 0; i < n; i++) {
+            nums[i] = sc.nextInt();
+        }
+
+        int k = sc.nextInt();
+
+        System.out.println(subarraySum(nums, k));
+    }
+}
